f2a7136a6478c2acba3eaf1d1f5a605938742bdf
SashaAkulich <todayfsh@gmail.com>
2026-04-29 18:42:07 +0300

    Update sheets.py.all

--- sheets.py
+++ sheets.py
@@ -2,21 +2,19 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 
+
 class GoogleSheetsManager:
     def __init__(self, sheet_id: str, credentials_json: str):
-        scope = ['https://spreadsheets.google.com/feeds', 
-                 'https://www.googleapis.com/auth/drive']
-        
-        # Преобразуем JSON строку в словарь
+        scope = [
+            'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive'
+        ]
         creds_dict = json.loads(credentials_json)
-        
-        # Создаем креды из словаря
         creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-        
         self.client = gspread.authorize(creds)
         self.sheet = self.client.open_by_key(sheet_id).sheet1
 
-  def log_lead(self, telegram_id: int, username: str, utm_data: dict, visit_id: str):
+    def log_lead(self, telegram_id: int, username: str, utm_data: dict, visit_id: str):
         row = [
             telegram_id,
             username or '',
